d18fdf4ac4b70d8440e6b509f87930c96b5444f5
Alex Preoteasa <apreoteasa@1wa.org>
2024-04-21 21:20:45 -0700

    2nd commit

---
+++ src/test/java/docker/build/SpringBootGithubDockerBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package docker.build;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGithubDockerBuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
